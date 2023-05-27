6f2e07f9cb5c53814825798bf1be04c8d84a73b0
riovitoria <50123899+riovitoria@users.noreply.github.com>
2023-05-27 18:44:05 -0300

    Delete Sistema E Commerce - módulo cadastro itens nos depósitos directory

--- Sistema E Commerce - módulo cadastro itens nos depósitos/src/main/webapp/scripts/confirmador.js
+++
@@ -1,7 +0,0 @@
- function confirmar(idcon){
-	 let resposta = confirm("Confirma a exclusão deste registro ?")
-	 if (resposta === true){
-	 	//alerta(idcon)
-	 	window.location.href = "delete?idcon=" +idcon
- 	}
- }
